99128d1437778bfe7de417d6022f000e2d03224d
Jakub Cabala <kuba.cabala6@gmail.com>
2023-12-06 20:36:37 +0000

    Common words finder implemented

--- 4.Lists/exercises.hs
+++ 4.Lists/exercises.hs
@@ -1,4 +1,7 @@
+import Data.List (sort)
+import Data.Char
 
+{--- Triads generator ---}
 divisors :: Int -> [Int]
 divisors x = [d | d <- [2..x-1], mod x d == 0]
 
@@ -21,9 +24,24 @@ triads n = [(x, y, z) | x <- [1..m], y <- [x+1..n],
                         z <- [y+1..n], x*x + y*y == z*z]
             where m = floor (fromIntegral n / sqrt 2)
 
+{--- Playing with list functions ---}
+
 nondec :: Ord a => [a] -> Bool
 nondec xs = and (zipWith (<=) xs (tail xs))
 
 position :: (Eq a) => a -> [a] -> Int
 position x xs
     = head ([j | (j,y) <- zip [0..] xs, y == x] ++ [-1])
+
+{--- Common words ---}
+commonWords :: Int -> [Char] -> [Char]
+commonWords n = concat . map showRun . take n . reverse . sort .
+                countRuns . sort . words . map toLower
+
+showRun :: (Int, [Char]) -> [Char]
+showRun (n, w) = w ++ ": " ++ show n ++ "\n"
+
+countRuns :: [[Char]] -> [(Int, [Char])]
+countRuns [] = []
+countRuns (w:ws) = (1 + length us, w):countRuns vs
+                   where (us, vs) = span (==w) ws
